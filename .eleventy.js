f3d07d659c19a1b2f8f00fdccc0b87b9e39927db
Pandu Supriyono <mail@supriyono.nl>
2021-01-21 14:58:58 +0100

    chore: node-watch implemented to watch sass dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,23 @@
-const sassWatch = require('./sass-watch')
+const sassWatch = require("./sass-watch");
+const fs = require("fs");
 
 module.exports = function (eleventyConfig) {
-  if (process.argv.includes('--watch')) {
+  if (process.argv.includes("--watch")) {
     // Watch Sass directory for updates.
-    sassWatch('./src/_sass/_main.scss', './dist/css/main.css');
+    sassWatch("./src/_sass/_main.scss", "./dist/css/main.css");
     // Refresh the browser when there are updates in the Sass directory.
-    eleventyConfig.addWatchTarget('./src/_sass/');
+    eleventyConfig.addWatchTarget("./src/_sass/**/*");
   }
 
-  eleventyConfig.addPassthroughCopy('dist')
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("dist");
   return {
     passthroughFileCopy: true,
     dir: {
-      input: 'src',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
-    },
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
   };
 };
