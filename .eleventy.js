d00bf2522ace763ce5ac1172704b5a7b61d2dce2
Pandu Supriyono <mail@supriyono.nl>
2021-02-24 14:01:14 +0100

    chore: sass files get compiled to css in right directory and fontawesome added

--- .eleventy.js
+++ .eleventy.js
@@ -4,13 +4,13 @@ const fs = require("fs");
 module.exports = function (eleventyConfig) {
   if (process.argv.includes("--watch")) {
     // Watch Sass directory for updates.
-    sassWatch("./src/_sass/_main.scss", "./dist/css/main.css");
+    sassWatch("./src/_sass/main.scss", "./src/css/main.css");
     // Refresh the browser when there are updates in the Sass directory.
     eleventyConfig.addWatchTarget("./src/_sass/**/*");
   }
 
+  eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy("dist");
   return {
     passthroughFileCopy: true,
     dir: {
